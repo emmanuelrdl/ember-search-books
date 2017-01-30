9990db6e6099385eafefa28860f0a6d00a0d16f6
emmanuelrdl <emmanuel.rudelle@hotmail.fr>
2017-01-30 18:24:53 +0100

    first commit

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,9 +2,22 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import Ember from 'ember';
 import DS from 'ember-data';
 
-export default JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
   // Application specific overrides go here
   host: 'https://www.googleapis.com',
-  namespace: '/books/v1/'
-  
+  urlForFindAll() {
+    return 'https://www.googleapis.com/books/v1/volumes?q=paris';
+  },
+
+  findAll(coucu) {
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        Ember.$.getJSON('https://www.googleapis.com/books/v1/volumes?q=paris', '').then(function(data) {
+          resolve(data);
+        }, function(jqXHR) {
+          reject(jqXHR);
+        });
+      });
+    }
+
+
 });
--- app/routes/books.js
+++ app/routes/books.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-  return this.store.query('volumes', {q: 'Paris'});
-}
+
+    return Ember.$.ajax({
+      url: "https://www.googleapis.com/books/v1/volumes?q=paris",
+      dataType: "jsonp",
+      error: function(xhr, status, error) {
+         console.log("Error");
+         console.log(xhr.statusText);
+         console.log(xhr.responseText);
+         console.log(xhr.status);
+         console.log(error);
+     }
+   });
+  }
 });
+
+// http://ippon.developpez.com/tutoriels/javascript/ember-et-contre-tous/
